e53783b8c63a621a610e3d174ac2bd17b5c1fbed
Ronin-new-laptop <likeemnow@gmail.com>
2024-02-06 21:00:58 +0800

    added reacttypescript and tailwind, modified models with json serializer

--- backend/models/models.go
+++ backend/models/models.go
@@ -2,9 +2,13 @@ package models
 
 import "gorm.io/gorm"
 
+type Credentials struct {
+	Email    string `gorm:"type:varchar(100);unique;not null" json:"email"`
+	Password string `gorm:"type:varchar(50);not null" json:"password"`
+}
+
 type User struct {
 	gorm.Model
-	Name     string `gorm:"type:varchar(100);not null"`
-	Email    string `gorm:"type:varchar(100);unique;not null"`
-	Password string `gorm:"type:varchar(50);not null"`
+	Name string `gorm:"type:varchar(100);not null" json:"name"`
+	Credentials
 }
